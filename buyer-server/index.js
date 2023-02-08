4427a42784626ffe7a2c0235f76a57c2c8f0b5f5
Gul Akberova <kbrovagul0@gmail.com>
2023-02-08 19:34:53 +0400

    nodemailer

--- buyer-server/index.js
+++ buyer-server/index.js
@@ -2,12 +2,14 @@ const express=require('express')
 const cors=require('cors')
 const { urlencoded } = require('express');
 const { default: mongoose } = require('mongoose');
+const nodemailer = require("nodemailer");
 require("dotenv").config()
 const productRouter= require('./routes/ProductRouter')
 const buyerRouter= require('./routes/buyerRouter')
 const categoryRouter= require('./routes/categoryRouter')
 const addressRouter=require('./routes/AddressRouter')
-const userRouter=require('./routes/UserRouter')
+const userRouter=require('./routes/UserRouter');
+const { options } = require('./routes/ProductRouter');
 const app=express()
 // const checkjwt=require('./auth/auth')
 
@@ -15,6 +17,7 @@ const PORT = process.env.POST || 3000;
 app.use(express.json())
 app.use(urlencoded())
 app.use(cors())
+
 
 // app.use(function(req,res,next){
 //     if(false){
@@ -46,5 +49,43 @@ app.use('/api/login',userRouter)
 // app.get('/check',checkjwt, function(req,res,next){
 //   res.send('selam dunya')
 // })
+
+
+
+ 
+   const transporter = nodemailer.createTransport({
+   
+     service:"gmail",
+    auth: {
+      user: 'kbrovagul0@gmail.com', // generated ethereal user
+      pass: 'Gul342155899159' // generated ethereal password
+    },
+  });
+
+  const info= {
+    from: 'kbrovagul0@gmail.com', // sender address
+    to: 'webdevelopernicat@gmail.com', // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+
+  }
+
+ transporter.sendMail(info, (err)=>{
+     if(err){
+      console.log(err)
+     
+     }else{
+     console.log('email sent')
+
+     }
+ })
+
+  // console.log("Message sent: " + info.messageId);
+  // console.log(info.accepted)
+  // console.log(rejected)
+
+
+// main().catch(e=>console.log(e))
   
 app.listen(PORT);
